9f3efed4d0c0a0e8e167108d267f50e057090160
Abdullah AlZahabi <a.alzahabi@ejad.solutions>
2024-04-29 22:03:46 +0300

    first-commit

---
+++ layouts/auth.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="bg-black mi-h-screen">
+    <div class="container pt-10">
+      <Nav />
+    </div>
+    <div class="flex justify-between items-start mt-24 container !pr-0">
+      <LoginCard class="w-[600px] px-6 py-6 rounded-3xl" />
+      <div class="sticky !-right-16 max-w-[70%]">
+        <img src="/images/login.svg" class="mt-36" />
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts"></script>
---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="bg-black h-screen">
+    <div class="container pt-10">
+      <Nav />
+      <slot />
+    </div>
+  </div>
+</template>
+<script setup lang="ts"></script>
